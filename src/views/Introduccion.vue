2b17a8864add75ea1c03fea7d76a031b47441700
maronita <maronita@users.noreply.github.com>
2021-06-18 20:25:12 -0500

    Intro

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -8,11 +8,19 @@
         .h3
           i.fas.fa-info
       .h3 Introducción
-    
-    figure.mb-5
-      .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption Video: Leyenda del video
+
+    .row.justify-content-center.align-items-center.mt-5
+      .col-12.col-lg-6.mb-5
+        p.mb-3 Bienvenido a este componente, en donde se abordan los temas: documentos contables y no contables, libros contables, archivo y conservación de documentos.
+
+        p.mb-3 Los soportes son muy importantes porque contienen la voluntad de las partes, reflejando el acuerdo comercial de la transacción económica, acorde con la normatividad vigente y lineamientos establecidos. Las temáticas que se desarrollan en esta unidad son las siguientes:
+
+        p 1. Documentos
+        p 1.1Contables, no contables, extracontables y títulos valores.
+        p 1.2 Definición, clasificación, características y diligenciamiento.
+
+      .col-12.col-lg-6
+        img(src="@/assets/curso/images/tema1/intro.svg" alt="", style="width:400px; display:block; margin:0 auto;")
     
 
 </template>
